1a7d2ca5411e30d6a29722230d2c4648196374e4
sreedhar <sreedhar70755@gmail.com>
2023-08-04 10:02:38 +0530

    ansible code 1

--- vars/main.tf
+++ vars/main.tf
@@ -6,8 +6,10 @@ output "name_of_student" {
   value = var.students
 }
 
-
+variable "students__m" {
+  default = [ "sridhar" , "vish" ]
+}
 output "name_of__student" {
-  value = var.students
+  value = var.students__m
 }
 
