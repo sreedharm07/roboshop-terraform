9f6725f7898c09c5667f74b7fe94ed29d7aca6fa
sreedhar <sreedhar70755@gmail.com>
2023-08-04 10:10:42 +0530

    ansible code 1

--- vars/main.tf
+++ vars/main.tf
@@ -1,17 +1,30 @@
-variable "students" {
-  default = "sridhar"
+## plan variable-----------------------
+#variable "students" {
+#  default = "sridhar"
+#}
+#
+#output "name_of_student" {
+#  value = var.students
+#}
+#
+## list variables ---------------------
+#variable "students__m" {
+#  default = [
+#  "sridhar" ,
+#  "vish" ]
+#}
+#output "name_of__student" {
+#  value = var.students[1]
+#}
+
+# mapping variable ------------------
+variable "students_m" {
+  default = {
+    sridhar = student
+    vish    = employee
+  }
 }
 
-output "name_of_student" {
-  value = var.students
+output "type_of_job" {
+  value = var.students_m["vish"]
 }
-
-variable "students__m" {
-  default = [
-  "sridhar" ,
-  "vish" ]
-}
-output "name_of__student" {
-  value = var.students
-}
-
