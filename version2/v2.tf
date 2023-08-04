e63d512b8c6312f1ef757af47cff31e6724ff285
sreedhar <sreedhar70755@gmail.com>
2023-08-04 20:59:37 +0530

    tf

--- version2/v2.tf
+++ version2/v2.tf
@@ -1,8 +1,8 @@
 variable  "components" {
   default = {
-    frontend    = "frontend-dev"
-    mongodb     = "mongodb-dev"
-    catalogue   = "catalogue-dev"
+    frontend    = {name = "frontens-dev"}
+    mongodb     = {name = "mongodb-dev"}
+    catalogue   =  "catalogue-dev"
 #      redis     = {name = "redis-dev"}
 #      user      = {name = "user-dev"}
 #      cart      = {name = "cart-dev"}
