d6da1e84500e1c42b83e3718fcd95a06932c21c4
sreedhar <sreedhar70755@gmail.com>
2023-08-04 16:16:56 +0530

    ansible code 1

---
+++ version2/v2.tf
@@ -0,0 +1,35 @@
+variable "components" {
+  frontend  = {}
+  mongodb   = {}
+  catalogue = {}
+#  redis     = {}
+#  user      = {}
+#  cart      = {}
+#  mysql     = {}
+#  shipping  = {}
+#  rabbitmq  = {}
+#  payment   = {}
+#  dispatch  = {}
+}
+
+resource "aws_instance" "instances" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-03c71c5d008981a14" ]
+
+  for_each= var.components
+tags = {
+    Name = lookup(each.key, null)
+  }
+}
+
+#
+#resource "aws_route53_record" "www" {
+#  zone_id = aws_route53_zone.primary.zone_id
+#  name    = "www.example.com"
+#  type    = "A"
+#  ttl     = 300
+#  records = [aws_eip.lb.public_ip]
+#}
+
+
