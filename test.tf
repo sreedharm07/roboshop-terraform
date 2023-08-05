d6e4c8910f876da41d439bab2d41ab38aa957b51
sreedhar <sreedhar70755@gmail.com>
2023-08-05 21:14:06 +0530

    tf

---
+++ test.tf
@@ -0,0 +1,18 @@
+variable "components" {
+  default = {
+    frontend = {name=frontend}
+    mongodb = {name=mongodb}
+    catalogue = {name=catalogue}
+    redis = {name=redis}
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = each.value
+  }
+}
