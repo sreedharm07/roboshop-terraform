01bb46c7440ba56b4f4751903484f152ecaf4db2
sreedhar <sreedhar70755@gmail.com>
2023-08-05 19:11:54 +0530

    tf

--- version3/modules/child.tf
+++ version3/modules/child.tf
@@ -3,27 +3,30 @@ data "aws_ami" "example" {
   owners           = [973714476881]
 }
 
-
-resource "aws_instance" "instance" {
-  ami           = data.aws_ami.example["id"]
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "frontend"
-  }
+output "test" {
+  value = "data.aws_ami.example"
 }
 
-output "front" {
-  value = aws_instance.instance
-}
-
-#resource "aws_route53_record" "dns" {
-#  zone_id = Z07380001ED1GOXY0KMLD
-#  name    = "frontend-dev.cloudev7.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = []
+#resource "aws_instance" "instance" {
+#  ami           = data.aws_ami.example
+#  instance_type = var.instance_type
+#
+#  tags = {
+#    Name = "frontend"
+#  }
 #}
-
-variable "instance_type" {}
+#
+#output "front" {
+#  value = aws_instance.instance
+#}
+#
+##resource "aws_route53_record" "dns" {
+##  zone_id = Z07380001ED1GOXY0KMLD
+##  name    = "frontend-dev.cloudev7.online"
+##  type    = "A"
+##  ttl     = 30
+##  records = []
+##}
+#
+#variable "instance_type" {}
 #variable "components" {}
