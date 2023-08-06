f066963d58e6af81de429d9c3eb8917c09ccceba
sreedhar <sreedhar70755@gmail.com>
2023-08-06 10:37:02 +0530

    tf

--- version3/modules/main.tf
+++ version3/modules/main.tf
@@ -3,6 +3,16 @@ data "aws_ami" "ami" {
   owners           = [973714476881]
   }
 
+resource "aws_instance" "instances" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.sec_id
+
+}
+
 output "ami" {
   value = data.aws_ami.ami
 }
+
+variable "instance_type" {}
+variable "sec_id" {}
--- version3/v3.tf
+++ version3/v3.tf
@@ -1,8 +1,18 @@
 module "servers" {
   source = "./modules"
-
+instance_type = var.instance_type
+ sec_id= var.id
 }
 
 output "test" {
-  value = module.servers.ami
+  value = module.servers
 }
+
+variable "instance_type" {
+  instance_type = "t3.micro"
+}
+
+variable "id" {
+ security_group =  "sg-03c71c5d008981a14"
+}
+
