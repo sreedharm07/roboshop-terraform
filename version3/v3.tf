2a8d8a9b202e216626e1da45e92998ad18c8cc45
sreedhar <sreedhar70755@gmail.com>
2023-08-06 17:04:59 +0530

    tf

--- version3/v3.tf
+++ version3/v3.tf
@@ -3,6 +3,6 @@ module "servers" {
  security_id= var.id
   name= each.value["name"]
   for_each = var.components
-  instance_type = var.components["instance_type"]
+  instance_type = each.value["instance_type"]
 }
 
