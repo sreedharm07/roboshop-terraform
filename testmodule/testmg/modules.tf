66523d272f40da52e9b885add53c452c001ba584
sreedhar <sreedhar70755@gmail.com>
2023-08-07 13:13:14 +0530

    tf

--- testmodule/testmg/modules.tf
+++ testmodule/testmg/modules.tf
@@ -28,7 +28,7 @@ resource "null_resource" "ansible" {
     cd /home/centos/roboshop-ansible
     git pull
     sleep 30
-    ansible-playbook -i ${var.name}-dev.cloudev7.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component= ${var.name}
+    ansible-playbook -i ${var.name}-dev.cloudev7.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 
 EOF
   }
