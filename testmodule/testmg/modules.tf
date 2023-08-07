27c7d38f7249ca8d80cba1776a92b64cdd042fd1
sreedhar <sreedhar70755@gmail.com>
2023-08-07 07:49:54 +0530

    tf

--- testmodule/testmg/modules.tf
+++ testmodule/testmg/modules.tf
@@ -8,3 +8,14 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_route53_record" "dns" {
+  zone_id = var.record
+  name    = "${var.name}-dev.cloudev7.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.web.private_ip ]
+}
+
+variable "record" {
+  default = "Z07380001ED1GOXY0KMLD"
+}
--- testmodule/vars.tf
+++ testmodule/vars.tf
@@ -8,6 +8,42 @@ variable "name" {
       name          = "mongodb"
       instance_type = "t1.micro"
     }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t1.micro"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t1.micro"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t1.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t1.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t1.micro"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t1.micro"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t1.micro"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t1.micro"
+    }
+    dispatch = {
+      name          = "dispatch"
+      instance_type = "t1.micro"
+    }
   }
 }
 
