999ec00bea8a9156b32df710f2eee333b5855655
sreedhar <sreedhar70755@gmail.com>
2023-08-07 09:54:01 +0530

    tf

--- testmodule/testmg/modules.tf
+++ testmodule/testmg/modules.tf
@@ -1,19 +1,25 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [var.security_id ]
+  ami                       = data.aws_ami.ami.id
+  instance_type             = var.instance_type
+  vpc_security_group_ids    = [var.security_id ]
 
   tags = {
-    Name = var.name
+    Name  = var.name
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 30"
   }
 }
 
+
+
 resource "aws_route53_record" "dns" {
-  zone_id = var.record
-  name    = "${var.name}-dev.cloudev7.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.web.private_ip ]
+  zone_id      = var.record
+  name         = "${var.name}-dev.cloudev7.online"
+  type         = "A"
+  ttl          = 30
+  records      = [aws_instance.web.private_ip ]
 }
 
 variable "record" {
