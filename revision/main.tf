b7f6679ad4b033841f92fd3b114fd806a6209b21
sreedhar <sreedhar70755@gmail.com>
2023-08-14 21:35:31 +0530

    tf

--- revision/main.tf
+++ revision/main.tf
@@ -1,9 +1,8 @@
 data "aws_ami" "ami" {
-  most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
+  owners           = [973714476881]
 }
 
-output "ami" {
+output "amiid" {
   value = "aws_ami"
 }
