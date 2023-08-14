1a48fe46fb6a35190d64e843edf93058e0b9afe2
sreedhar <sreedhar70755@gmail.com>
2023-08-14 22:03:25 +0530

    tf

--- revi/main.tf
+++ revi/main.tf
@@ -12,3 +12,11 @@ resource "aws_instance" "instance" {
     Name = "frontend"
   }
 }
+
+resource "aws_route53_record" "adding_record" {
+  zone_id = "Z07380001ED1GOXY0KMLD"
+  name    = "frontend"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
