41bff401e55b08284ff0bcc68477f1d116a2d339
sreedhar <sreedhar70755@gmail.com>
2023-08-15 06:01:58 +0530

    tf

--- revi/main.tf
+++ revi/main.tf
@@ -27,7 +27,7 @@ resource "aws_route53_record" "record" {
   name    = lookup(each.value,"name",null )
   type    = "A"
   ttl     = 30
-  records = [lookup(lookup(aws_instance.instances, each.key,null), private_ip, null)]
+  records = [lookup(lookup(aws_instance.instances, each.key,null), "private_ip", null)]
 }
 
 
