2a647dfe2b88fe18f4f1473b0f29cf3c99330ce7
sreedhar <sreedhar70755@gmail.com>
2023-08-15 12:35:38 +0530

    tf

--- revi/main.tf
+++ revi/main.tf
@@ -36,7 +36,5 @@ resource "aws_route53_record" "record" {
   name    ="frontend"
   type    = "A"
   ttl     = 30
-  records = [lookup(lookup(aws_instance.instances,each.key,null),private_ip, null)]
+  records = aws_instance.instances.private_ip       #[lookup(lookup(aws_instance.instances,each.key,null),private_ip, null)]
 }
-
-
