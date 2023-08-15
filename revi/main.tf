214f4ed2e8bb496cb692deccaf61d3ef74d06e35
sreedhar <sreedhar70755@gmail.com>
2023-08-15 13:54:26 +0530

    tf

--- revi/main.tf
+++ revi/main.tf
@@ -21,4 +21,13 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+
+}
+resource "aws_route53_record" "record" {
+  for_each = var.components
+  zone_id = "Z07380001ED1GOXY0KMLD"
+  name    = each.value["name"]
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.instance.private_ip ]
 }
