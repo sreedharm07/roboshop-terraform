32c12cc9c3594dfe2907a221bdd9e7fe9875c25d
sreedhar <sreedhar70755@gmail.com>
2023-08-15 06:01:08 +0530

    tf

--- revi/main.tf
+++ revi/main.tf
@@ -1,56 +1,34 @@
+variable "components" {
+  default = {
+    frontend   = {name="frontend"}
+    mongodb    = {name="mongodb"}
+    catalogue  = {name="catalogue"}
+  }
+}
+
 data "aws_ami" "ami" {
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = [973714476881]
 }
 
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instances" {
   ami           = data.aws_ami.ami.id
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = ["sg-03c71c5d008981a14" ]
+ for_each = var.components
   tags = {
-    Name = "frontend"
+    Name = lookup(each.value,"name",null )
   }
 }
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "record" {
+  for_each = var.components
   zone_id = "Z07380001ED1GOXY0KMLD"
-  name    = "frontend"
+  name    = lookup(each.value,"name",null )
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [lookup(lookup(aws_instance.instances, each.key,null), private_ip, null)]
 }
 
 
 
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z07380001ED1GOXY0KMLD"
-  name    = "mongodb"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z07380001ED1GOXY0KMLD"
-  name    = "catalogue"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
