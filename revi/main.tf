97291b207b5483931dfddd875f73b4b646728da9
sreedhar <sreedhar70755@gmail.com>
2023-08-15 05:30:13 +0530

    tf

--- revi/main.tf
+++ revi/main.tf
@@ -12,10 +12,44 @@ resource "aws_instance" "instance" {
     Name = "frontend"
   }
 }
-
 resource "aws_route53_record" "adding_record" {
   zone_id = "Z07380001ED1GOXY0KMLD"
   name    = "frontend"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
+
+
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_route53_record" "adding_record" {
+  zone_id = "Z07380001ED1GOXY0KMLD"
+  name    = "mongodb"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+resource "aws_route53_record" "adding_record" {
+  zone_id = "Z07380001ED1GOXY0KMLD"
+  name    = "catalogue"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
