5c75a5be97a378e367a0fdb73cf8063045ef165c
sreedhar <sreedhar70755@gmail.com>
2023-08-15 17:09:20 +0530

    tf

--- revi/modules/main.tf
+++ revi/modules/main.tf
@@ -16,3 +16,17 @@ resource "aws_route53_record" "records" {
   zone_id = var.zone_id
   records = [aws_instance.instances.private_ip ]
 }
+
+resource "null_resource" "ansible" {
+  provisioner "local-exec" {
+    depends_on=[
+    aws_route53_record.records ]
+    command = <<EOF
+cd /home/centos/roboshop-terraform/revi/learn-ansible
+git pull
+sleep 50
+ansible-playbook -i "${var.components}-dev.cloudev.online", main.yml -i ansible_user=centos -i ansible_password=DevOps321 -i var.components
+
+EOF
+  }
+}
