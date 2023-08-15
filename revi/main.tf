d08353c59ba79fbaf9af1f4e07926ce275099958
sreedhar <sreedhar70755@gmail.com>
2023-08-15 12:34:04 +0530

    tf

--- revi/main.tf
+++ revi/main.tf
@@ -7,6 +7,13 @@ variable "security_group" {
 variable "zone_id" {
   default = "Z07380001ED1GOXY0KMLD"
 }
+variable "components" {
+  default = {
+    frontend= {name="frontend"}
+    mongodb= {name="mongodb"}
+    catalogue={name="catalogue"}
+  }
+}
 
 data "aws_ami" "ami" {
   name_regex       = "Centos-8-DevOps-Practice"
@@ -18,16 +25,18 @@ resource "aws_instance" "instances" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [var.security_group]
+  for_each = var.components
   tags = {
-    Name = "frontend"
+    Name = each.key
   }
 }
 resource "aws_route53_record" "record" {
+  for_each = var.components
   zone_id = var.zone_id
   name    ="frontend"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.instances.private_ip]
+  records = [lookup(lookup(aws_instance.instances,each.key,null),private_ip, null)]
 }
 
 
