d0af58f19d3a96923e05798d70384a78e59a1ea8
sreedhar <sreedhar70755@gmail.com>
2023-08-15 20:36:10 +0530

    tf

--- revi/modules/main.tf
+++ revi/modules/main.tf
@@ -26,6 +26,7 @@ resource "null_resource" "ansible" {
     command = <<EOF
      cd /root/learn-ansible
      git pull
+     sleep 30
      ansible-playbook -i ${var.components}-dev.cloudev7.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.components}
 EOF
   }
