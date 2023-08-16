e8093f73eaa9fe38cfa8a02fc4e9593e8209da66
sreedhar <sreedhar70755@gmail.com>
2023-08-16 10:42:20 +0530

    tf

--- revivars/main.tf
+++ revivars/main.tf
@@ -1,8 +1,10 @@
 module "servers" {
-  source = "git::https://github.com/sreedharm07/terraform-module.git"
-  components = each.value["name"]
-  security_group = var.security_group
-  instance_type = var.instance_type
-  zone_id = var.zone_id
-  for_each = var.components
+
+  source          = "git::https://github.com/sreedharm07/roboshop-terraform.git/revi/modules"
+
+  components      = each.value["name"]
+  security_group  = var.security_group
+  instance_type   = var.instance_type
+  zone_id         = var.zone_id
+  for_each        = var.components
 }
