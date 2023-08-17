d375dc6049720135a2a679d2223a20a715c43f1e
sreedhar <sreedhar70755@gmail.com>
2023-08-17 19:20:47 +0530

    tf

--- prac/module/main.tf
+++ prac/module/main.tf
@@ -24,7 +24,7 @@ resource "null_resource" "null" {
      cd /root/learn-ansible
      git pull
      sleep 30
-     ansible-playbook -i ${var.components}-dev.cloudev7.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.components}
+     ansible-playbook -i ${var.components}.cloudev7.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.components}
 EOF
   }
 }
